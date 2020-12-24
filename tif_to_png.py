d78f3de78164c655118a7a18b06c88ba3cce5f35
aggiungi1procione <65766418+aggiungi1procione@users.noreply.github.com>
2020-12-24 15:44:36 +0100

    Update tif_to_png.py

--- tif_to_png.py
+++ tif_to_png.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
 Created on Tue Jun 16 09:52:54 2020
 
 @author: Bibs
@@ -11,18 +9,18 @@ import math
 
 #-tif to png converter-
 
-for file in glob.glob('E:/Bianka_Thesis/images_tif/*.tif'):
+for file in glob.glob('PATH/images_tif/*.tif'):
     img = Image.open(file)                                                      
     img.save(file.replace('tif', 'png'))
 
 #-images to tiles slicer-
 tile_dim = 800
 img_no=0
-for file in glob.glob('E:/Bianka_Thesis/images_png/*.png'):
+for file in glob.glob('PATH/images_png/*.png'):
     img = Image.open(file)   
     img_no+=1
     H_slider = int(tile_dim-((tile_dim-(img.height%tile_dim))/(img.height//tile_dim)))
-    W_slider = int(tile_dim-((tile_dim-(img.width%tile_dim))/(img.width//tile_dim)))       #int -> rounding down (can cut off some pixels, but at least won't "create" more)
+    W_slider = int(tile_dim-((tile_dim-(img.width%tile_dim))/(img.width//tile_dim)))      
     
     x=0
     y=0
@@ -30,7 +28,7 @@ for file in glob.glob('E:/Bianka_Thesis/images_png/*.png'):
     for i in range(int((img.height-tile_dim)/H_slider)+1):
         for j in range(int((img.width-tile_dim)/W_slider)+1):
             img_crop=img.crop((x,y,x+tile_dim,y+tile_dim))
-            img_crop.save('E:/Bianka_Thesis/dataset_tiles/img'+ str(img_no) +'tile'+ str(tile_no) +'.png')
+            img_crop.save('PATH/dataset_tiles/img'+ str(img_no) +'tile'+ str(tile_no) +'.png')
             x+=W_slider 
             j+=1
             tile_no+=1
